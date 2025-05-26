0684e7a7df32a718e752296404dbbf727ac75ce4
KristersPuzulis231RDB146 <102966224+KristersPuzulis@users.noreply.github.com>
2025-05-26 21:43:45 +0300

    Update Color.kt

--- app/src/main/java/com/example/towerdefense/ui/theme/Color.kt
+++ app/src/main/java/com/example/towerdefense/ui/theme/Color.kt
@@ -1,7 +1,11 @@
+// Color.kt
+// Šis fails satur visas spēlē izmantotās krāsas – gan tēmas noklusējuma krāsas, gan pielāgotas vizuālajām daļām
+
 package com.example.towerdefense.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Noklusējuma krāsas
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -10,8 +14,9 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val GrassColor = Color(0xFFB9FBC0)
-val PathColor = Color(0xFFDAB785)
-val TowerColor = Color.DarkGray
-val HPBarRed = Color.Red
-val HPBarGreen = Color.Green
+// Pielāgotas spēles vizuālās krāsas
+val GrassColor = Color(0xFFB9FBC0) // Spēles pamata fons – zāle
+val PathColor = Color(0xFFDAB785) // Ceļš, pa kuru pārvietojas pretinieki
+val TowerColor = Color.DarkGray // Torņu krāsa, pirms tika ievietots attēls priekš torņa
+val HPBarRed = Color.Red // Dzīvības joslas sarkanā daļa 
+val HPBarGreen = Color.Green // Dzīvības joslas zaļā daļa
