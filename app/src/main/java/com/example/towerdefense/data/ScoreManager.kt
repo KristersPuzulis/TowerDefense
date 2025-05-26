d263b6db4e025458cdcb6a24b3491b991e7894f9
KristersPuzulis231RDB146 <102966224+KristersPuzulis@users.noreply.github.com>
2025-05-26 21:33:03 +0300

    Update ScoreManager.kt

--- app/src/main/java/com/example/towerdefense/data/ScoreManager.kt
+++ app/src/main/java/com/example/towerdefense/data/ScoreManager.kt
@@ -1,11 +1,17 @@
+// ScoreManager.kt
+// Saglabā visus spēlētāju rezultātus 
+
 package com.example.towerdefense.data
 
 object ScoreManager {
+    // Saraksts, kurā tiek uzglabāti pāri - spēlētāja vārds un rezultāts
     private val scores = mutableListOf<Pair<String, Int>>()
 
+    // Pievieno jaunu rezultātu sarakstam
     fun addScore(playerName: String, score: Int) {
         scores.add(playerName to score)
     }
 
+    // Atgriež visus rezultātus
     fun getAllScores(): List<Pair<String, Int>> = scores
 }
