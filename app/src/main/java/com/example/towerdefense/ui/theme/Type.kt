3fa09d2a6631aa037f081d2708d4f0521a9867d8
KristersPuzulis231RDB146 <102966224+KristersPuzulis@users.noreply.github.com>
2025-05-26 22:03:36 +0300

    Update Type.kt

--- app/src/main/java/com/example/towerdefense/ui/theme/Type.kt
+++ app/src/main/java/com/example/towerdefense/ui/theme/Type.kt
@@ -1,3 +1,6 @@
+// Type.kt
+// Šis fails definē noklusēto teksta stilu, ko izmanto lietotnes dizaina tēma.
+
 package com.example.towerdefense.ui.theme
 
 import androidx.compose.material3.Typography
@@ -6,13 +9,13 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-
+// Noklusētais teksta stils lieliem tekstiem
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = FontFamily.Default, // Noklusētā fontu ģimene
+        fontWeight = FontWeight.Normal, // Fonta biezums
+        fontSize = 16.sp, // Fonta izmērs
+        lineHeight = 24.sp, // Attālums starp rindām
+        letterSpacing = 0.5.sp // Burtu atstarpe
     )
 )
