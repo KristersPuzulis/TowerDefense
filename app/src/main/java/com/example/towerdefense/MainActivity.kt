85164c9fab7e56e188c5f5639274b5d853e68ad2
KristersPuzulis231RDB146 <102966224+KristersPuzulis@users.noreply.github.com>
2025-05-26 21:24:21 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/towerdefense/MainActivity.kt
+++ app/src/main/java/com/example/towerdefense/MainActivity.kt
@@ -1,3 +1,6 @@
+// MainActivity.kt
+// Šis fails satur galveno aktivitāti, kas uzsāk spēli.
+
 package com.example.towerdefense
 
 import android.os.Bundle
@@ -9,11 +12,13 @@ import com.example.towerdefense.ui.theme.TowerDefenseTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Ļauj zīmēt saturu zem statusa joslām
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
+        // Iestata spēles saturu un dizaina tēmu
         setContent {
             TowerDefenseTheme {
-                AppNavigation()
+                AppNavigation() // Uzsāk spēles navigāciju starp ekrāniem
             }
         }
     }
